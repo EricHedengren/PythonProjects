de7e1c724a76b8f9d8fd3c2f850d9594b12e694d
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-05-02 13:16:07 -0600

    Delete PasswordGenerator.py

--- PasswordGenerator.py
+++
@@ -1,8 +0,0 @@
-import random
-letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*`~-+=';:/?\|><"
-password = ''
-
-for i in range(25):
-    password += letters[random.randint(0,len(letters))]
-
-print(password)

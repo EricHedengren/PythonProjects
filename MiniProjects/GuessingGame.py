e6f9b6cb2347fb0a54b71d63a7f0f2b1b9955f8c
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-07-12 19:12:47 -0600

    Count the guesses

--- MiniProjects/GuessingGame.py
+++ MiniProjects/GuessingGame.py
@@ -7,8 +7,10 @@ number = random.randint(x,y)
 
 print("Guess a number between " + str(x) + " and " + str(y) + ": ", end='')
 guess = int(input())
+guesses = 1
 
 while guess != number:
+    guesses += 1
     if guess > y or guess < x:
         print("Guess is out of range.\nGuess again: ",end='')
     elif guess > number:
@@ -17,4 +19,4 @@ while guess != number:
         print("Higher: ",end='')
     guess = int(input())
 
-print("You won!")
+print("You won! It took",guesses,"guesses.")

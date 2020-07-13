7ada381c4afe743440b13be6c66e51d24e06917a
Eric Hedengren <46898294+EricHedengren@users.noreply.github.com>
2020-07-12 18:53:34 -0600

    Fixed infinite maximum bug

--- MiniProjects/GuessForYou.py
+++ MiniProjects/GuessForYou.py
@@ -14,23 +14,20 @@ def run_guesser(min2, max2):
     while feedback != 'You Won':
         count += 1
         guess = int(((max2-min2)/2)+min2)
-        print(guess)
         feedback = higher_lower(guess)
-        print(feedback)
         if feedback == 'Too High':
             max2 = guess
         elif feedback == 'Too Low':
             min2 = guess
-
     return count
 
 min1 = 1
-max1 = 2**100
+max1 = 1000000
 
 num_guess = []
 
-for i in range(100000):
+for i in range(1000):
     number = randint(min1, max1)
-    num_guess.append(run_guesser(min1, max1))
+    num_guess.append(run_guesser(min1, max1+1))
 
 print(sum(num_guess)/len(num_guess))
